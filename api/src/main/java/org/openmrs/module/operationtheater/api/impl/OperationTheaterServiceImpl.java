83dca4dd32995d665fe8e631781c571be71b0bb0
pramidatumma <pramidat@thoughtworks.com>
2017-04-19 18:07:19 +0530

    Yugesh, Pramida|#411|Initial omod structure

---
+++ api/src/main/java/org/openmrs/module/operationtheater/api/impl/OperationTheaterServiceImpl.java
@@ -0,0 +1,52 @@
+/**
+ * This Source Code Form is subject to the terms of the Mozilla Public License,
+ * v. 2.0. If a copy of the MPL was not distributed with this file, You can
+ * obtain one at http://mozilla.org/MPL/2.0/. OpenMRS is also distributed under
+ * the terms of the Healthcare Disclaimer located at http://openmrs.org/license.
+ *
+ * Copyright (C) OpenMRS Inc. OpenMRS is a registered trademark and the OpenMRS
+ * graphic logo is a trademark of OpenMRS Inc.
+ */
+package org.openmrs.module.operationtheater.api.impl;
+
+import org.openmrs.api.APIException;
+import org.openmrs.api.UserService;
+import org.openmrs.api.impl.BaseOpenmrsService;
+import org.openmrs.module.operationtheater.Item;
+import org.openmrs.module.operationtheater.api.OperationTheaterService;
+import org.openmrs.module.operationtheater.api.dao.OperationTheaterDao;
+
+public class OperationTheaterServiceImpl extends BaseOpenmrsService implements OperationTheaterService {
+	
+	OperationTheaterDao dao;
+	
+	UserService userService;
+	
+	/**
+	 * Injected in moduleApplicationContext.xml
+	 */
+	public void setDao(OperationTheaterDao dao) {
+		this.dao = dao;
+	}
+	
+	/**
+	 * Injected in moduleApplicationContext.xml
+	 */
+	public void setUserService(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@Override
+	public Item getItemByUuid(String uuid) throws APIException {
+		return dao.getItemByUuid(uuid);
+	}
+	
+	@Override
+	public Item saveItem(Item item) throws APIException {
+		if (item.getOwner() == null) {
+			item.setOwner(userService.getUser(1));
+		}
+		
+		return dao.saveItem(item);
+	}
+}
